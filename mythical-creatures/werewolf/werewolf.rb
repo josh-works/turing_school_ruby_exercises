e98d074731345abc4fa49f13be7c0f46fc82b187
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-20 14:48:22 -0700

    some change to the file

--- mythical-creatures/werewolf/werewolf.rb
+++ mythical-creatures/werewolf/werewolf.rb
@@ -4,7 +4,7 @@ class Werewolf
   attr_accessor :name, :location, :form, :human, :werewolf
   attr_reader :victims
 
-  def initialize(name, location="earth", form="human")
+  def initialize(name, location = 'earth', form = 'human')
     @name = name
     @location = location
     @form = form
@@ -34,17 +34,12 @@ class Werewolf
 
   def feed(victim)
     if @werewolf == false
-      p "Hey dude. Why are you nibbling on me?"
+      p 'Hey dude. Why are you nibbling on me?'
       return false
     end
     victim.status = :dead
     @victims += 1
     @hungry = false
-    return "'AooOOoo' *chomp* 'AAUGHHH' *snarl* AAAH!!!"
+    "'AooOOoo' *chomp* 'AAUGHHH' *snarl* AAAH!!!"
   end
-
-
-
-
-
 end
